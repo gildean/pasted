c765e8b53c6c04f2cb88f11e881d0d481e181ab9
Olli K <gildean@salaliitto.com>
2013-04-22 02:33:24 +0300

    Change one indexOf to hasOwnProperty

--- public/js/script.js
+++ public/js/script.js
@@ -263,7 +263,7 @@
 
     function setCookie(cookieName, value) {
         var val;
-        if (Object.keys(cookies).indexOf(cookieName) > -1) {
+        if (cookies.hasOwnProperty(cookieName)) {
             document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT; path=/';
         }
         if (cookieName !== 'theme') {
